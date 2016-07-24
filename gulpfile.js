54511c468d03608c6e91343ed86743389915d956
David Dorward <david@dorward.me.uk>
2016-07-24 12:29:46 +0100

    Social networking links

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,15 @@ var autoprefix = require('gulp-autoprefixer');
 var changed = require('gulp-changed');
 var concat = require('gulp-concat');
 var imagemin = require('gulp-imagemin');
+var inject = require('gulp-inject');
+//var includer = require('gulp-html-ssi');
 var minifyCSS = require('gulp-minify-css');
 var minifyHTML = require('gulp-minify-html');
 var responsive = require("gulp-responsive");
+var svgmin = require('gulp-svgmin');
+var svgstore = require('gulp-svgstore');
+
+
 
 // default gulp task
 gulp.task('default', ['imagemin', 'htmlpage', 'styles'], function() {
@@ -28,7 +34,7 @@ gulp.task('default', ['imagemin', 'htmlpage', 'styles'], function() {
 
 // minify and resize new images
 gulp.task('imagemin', function() {
-	var imgSrc = './src/images/**/*',
+	var imgSrc = './src/images/**/*.jpeg',
 		imgDst = './build/images';
 
 	gulp.src(imgSrc)
@@ -48,17 +54,40 @@ gulp.task('imagemin', function() {
 		.pipe(gulp.dest(imgDst));
 });
 
-
-// minify new or changed HTML pages
+// SVG image
 gulp.task('htmlpage', function() {
-	var htmlSrc = './src/*.html',
-		htmlDst = './build';
 
-	gulp.src(htmlSrc)
+	var htmlSrc = './src/*.html';
+	var htmlDst = './build';
+    var svgs = gulp
+        .src('./src/images/icons/*.svg')
+        .pipe(svgstore({ inlineSvg: true }));
+
+
+    function fileContents (filePath, file) {
+        return file.contents.toString();
+    }
+
+	return gulp.src(htmlSrc)
 		.pipe(changed(htmlDst))
+		.pipe(inject(svgs, { transform: fileContents }))
 		.pipe(minifyHTML())
 		.pipe(gulp.dest(htmlDst));
+
 });
+
+
+// // minify new or changed HTML pages
+// gulp.task('htmlpage', function() {
+// 	var htmlSrc = './src/*.html',
+// 		htmlDst = './build';
+
+// 	gulp.src(htmlSrc)
+// 		.pipe(changed(htmlDst))
+// 		.pipe(includer())
+// 		.pipe(minifyHTML())
+// 		.pipe(gulp.dest(htmlDst));
+// });
 
 // CSS concat, auto-prefix and minify
 gulp.task('styles', function() {
